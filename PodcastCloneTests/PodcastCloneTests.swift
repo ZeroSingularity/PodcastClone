63f055450ed9a7a4ac00745d309179c0c8a83f0d
Jacobo Hernandez <zerosingularity@jacobos-macbook-pro.local>
2018-09-17 09:58:05 -0500

    Initial Commit

---
+++ PodcastCloneTests/PodcastCloneTests.swift
@@ -0,0 +1,36 @@
+//
+//  PodcastCloneTests.swift
+//  PodcastCloneTests
+//
+//  Created by Jacobo Hernandez on 9/17/18.
+//  Copyright © 2018 Jacobo Hernandez. All rights reserved.
+//
+
+import XCTest
+@testable import PodcastClone
+
+class PodcastCloneTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
